cca8e27e69c4ed0d5a40f407c2b548028243f8f4
HeeChul <khc4190@naver.com>
2020-11-06 06:14:34 +0000

    init

---
+++ client.js
@@ -0,0 +1,254 @@
+const mediasoup = require('mediasoup-client');
+const socketClient = require('socket.io-client');
+const socketPromise = require('./lib/socket.io-promise').promise;
+const config = require('./config');
+
+const hostname = window.location.hostname;
+
+let device;
+let socket;
+let producer;
+
+const $ = document.querySelector.bind(document);
+const $fsPublish = $('#fs_publish');
+const $fsSubscribe = $('#fs_subscribe');
+const $btnConnect = $('#btn_connect');
+const $btnWebcam = $('#btn_webcam');
+const $btnScreen = $('#btn_screen');
+const $btnSubscribe = $('#btn_subscribe');
+const $chkSimulcast = $('#chk_simulcast');
+const $txtConnection = $('#connection_status');
+const $txtWebcam = $('#webcam_status');
+const $txtScreen = $('#screen_status');
+const $txtSubscription = $('#sub_status');
+let $txtPublish;
+
+$btnConnect.addEventListener('click', connect);
+$btnWebcam.addEventListener('click', publish);
+$btnScreen.addEventListener('click', publish);
+$btnSubscribe.addEventListener('click', subscribe);
+
+if (typeof navigator.mediaDevices.getDisplayMedia === 'undefined') {
+  $txtScreen.innerHTML = 'Not supported';
+  $btnScreen.disabled = true;
+}
+
+async function connect() {
+  $btnConnect.disabled = true;
+  $txtConnection.innerHTML = 'Connecting...';
+
+  const opts = {
+    path: '/server',
+    transports: ['websocket'],
+  };
+
+  const serverUrl = `https://${hostname}`;
+  socket = socketClient(serverUrl, opts);
+  console.log(socket);
+  socket.request = socketPromise(socket);
+
+  socket.on('connect', async () => {
+    $txtConnection.innerHTML = 'Connected';
+    $fsPublish.disabled = false;
+    $fsSubscribe.disabled = false;
+
+    const data = await socket.request('getRouterRtpCapabilities');
+    await loadDevice(data);
+  });
+
+  socket.on('disconnect', () => {
+    $txtConnection.innerHTML = 'Disconnected';
+    $btnConnect.disabled = false;
+    $fsPublish.disabled = true;
+    $fsSubscribe.disabled = true;
+  });
+
+  socket.on('connect_error', (error) => {
+    console.error('could not connect to %s%s (%s)', serverUrl, opts.path, error.message);
+    $txtConnection.innerHTML = 'Connection failed';
+    $btnConnect.disabled = false;
+  });
+
+  socket.on('newProducer', () => {
+    $fsSubscribe.disabled = false;
+  });
+}
+
+async function loadDevice(routerRtpCapabilities) {
+  try {
+    device = new mediasoup.Device();
+  } catch (error) {
+    if (error.name === 'UnsupportedError') {
+      console.error('browser not supported');
+    }
+  }
+  await device.load({ routerRtpCapabilities });
+}
+
+async function publish(e) {
+  const isWebcam = (e.target.id === 'btn_webcam');
+  $txtPublish = isWebcam ? $txtWebcam : $txtScreen;
+
+  const data = await socket.request('createProducerTransport', {
+    forceTcp: false,
+    rtpCapabilities: device.rtpCapabilities,
+  });
+  console.log(data);
+  if (data.error) {
+    console.error(data.error);
+    return;
+  }
+
+  const transport = device.createSendTransport(data);
+  transport.on('connect', async ({ dtlsParameters }, callback, errback) => {
+    socket.request('connectProducerTransport', { dtlsParameters })
+      .then(callback)
+      .catch(errback);
+  });
+
+  transport.on('produce', async ({ kind, rtpParameters }, callback, errback) => {
+    try {
+      const { id } = await socket.request('produce', {
+        transportId: transport.id,
+        kind,
+        rtpParameters,
+      });
+      callback({ id });
+    } catch (err) {
+      errback(err);
+    }
+  });
+
+  transport.on('connectionstatechange', (state) => {
+    switch (state) {
+      case 'connecting':
+        $txtPublish.innerHTML = 'publishing...';
+        $fsPublish.disabled = true;
+        $fsSubscribe.disabled = true;
+      break;
+
+      case 'connected':
+        document.querySelector('#local_video').srcObject = stream;
+        $txtPublish.innerHTML = 'published';
+        $fsPublish.disabled = true;
+        $fsSubscribe.disabled = false;
+      break;
+
+      case 'failed':
+        transport.close();
+        $txtPublish.innerHTML = 'failed';
+        $fsPublish.disabled = false;
+        $fsSubscribe.disabled = true;
+      break;
+
+      default: break;
+    }
+  });
+
+  let stream;
+  try {
+    stream = await getUserMedia(transport, isWebcam);
+    const track = stream.getVideoTracks()[0];
+    const params = { track };
+    if ($chkSimulcast.checked) {
+      params.encodings = [
+        { maxBitrate: 100000 },
+        { maxBitrate: 300000 },
+        { maxBitrate: 900000 },
+      ];
+      params.codecOptions = {
+        videoGoogleStartBitrate : 1000
+      };
+    }
+    producer = await transport.produce(params);
+  } catch (err) {
+    $txtPublish.innerHTML = 'failed';
+  }
+}
+
+async function getUserMedia(transport, isWebcam) {
+  if (!device.canProduce('video')) {
+    console.error('cannot produce video');
+    return;
+  }
+
+  let stream;
+  try {
+    stream = isWebcam ?
+      await navigator.mediaDevices.getUserMedia({ video: true }) :
+      await navigator.mediaDevices.getDisplayMedia({ video: true });
+  } catch (err) {
+    console.error('getUserMedia() failed:', err.message);
+    throw err;
+  }
+  return stream;
+}
+
+async function subscribe() {
+  const data = await socket.request('createConsumerTransport', {
+    forceTcp: false,
+  });
+  if (data.error) {
+    console.error(data.error);
+    return;
+  }
+
+  const transport = device.createRecvTransport(data);
+  transport.on('connect', ({ dtlsParameters }, callback, errback) => {
+    socket.request('connectConsumerTransport', {
+      transportId: transport.id,
+      dtlsParameters
+    })
+      .then(callback)
+      .catch(errback);
+  });
+
+  transport.on('connectionstatechange', async (state) => {
+    switch (state) {
+      case 'connecting':
+        $txtSubscription.innerHTML = 'subscribing...';
+        $fsSubscribe.disabled = true;
+        break;
+
+      case 'connected':
+        document.querySelector('#remote_video').srcObject = await stream;
+        await socket.request('resume');
+        $txtSubscription.innerHTML = 'subscribed';
+        $fsSubscribe.disabled = true;
+        break;
+
+      case 'failed':
+        transport.close();
+        $txtSubscription.innerHTML = 'failed';
+        $fsSubscribe.disabled = false;
+        break;
+
+      default: break;
+    }
+  });
+
+  const stream = consume(transport);
+}
+
+async function consume(transport) {
+  const { rtpCapabilities } = device;
+  const data = await socket.request('consume', { rtpCapabilities });
+  const {
+    producerId,
+    id,
+    kind,
+    rtpParameters,
+  } = data;
+
+  let codecOptions = {};
+  const consumer = await transport.consume({
+    id,
+    producerId,
+    kind,
+    rtpParameters,
+    codecOptions,
+  });
+  const stream = new MediaStream();
+  stream.addTrack(consumer.track);
+  return stream;
+}
---
+++ server.js
@@ -0,0 +1,236 @@
+const mediasoup = require('mediasoup');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+const socketIO = require('socket.io');
+const config = require('./config');
+
+// Global variables
+let worker;
+let webServer;
+let socketServer;
+let expressApp;
+let producer;
+let consumer;
+let producerTransport;
+let consumerTransport;
+let mediasoupRouter;
+
+(async () => {
+  try {
+    await runExpressApp();
+    await runWebServer();
+    await runSocketServer();
+    await runMediasoupWorker();
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
+async function runExpressApp() {
+  expressApp = express();
+  expressApp.use(express.json());
+  expressApp.use(express.static(__dirname));
+
+  expressApp.use((error, req, res, next) => {
+    if (error) {
+      console.warn('Express app error,', error.message);
+
+      error.status = error.status || (error.name === 'TypeError' ? 400 : 500);
+
+      res.statusMessage = error.message;
+      res.status(error.status).send(String(error));
+    } else {
+      next();
+    }
+  });
+}
+
+async function runWebServer() {
+//  const { sslKey, sslCrt } = config;
+//  if (!fs.existsSync(sslKey) || !fs.existsSync(sslCrt)) {
+//    console.error('SSL files are not found. check your config.js file');
+//    process.exit(0);
+//  }
+//  const tls = {
+//    cert: fs.readFileSync(sslCrt),
+//    key: fs.readFileSync(sslKey),
+//  };
+  webServer = http.createServer(expressApp);
+  webServer.on('error', (err) => {
+    console.error('starting web server failed:', err.message);
+  });
+
+  await new Promise((resolve) => {
+    const { listenIp, listenPort } = config;
+    webServer.listen(listenPort, listenIp, () => {
+      const listenIps = config.mediasoup.webRtcTransport.listenIps[0];
+      const ip = listenIps.announcedIp || listenIps.ip;
+      console.log('server is running');
+      console.log(`open https://${ip}:${listenPort} in your web browser`);
+      resolve();
+    });
+  });
+}
+
+async function runSocketServer() {
+  socketServer = socketIO(webServer, {
+    serveClient: false,
+    path: '/server',
+    log: false,
+  });
+
+  socketServer.on('connection', (socket) => {
+    console.log('client connected');
+
+    // inform the client about existence of producer
+    if (producer) {
+      socket.emit('newProducer');
+    }
+
+    socket.on('disconnect', () => {
+      console.log('client disconnected');
+    });
+
+    socket.on('connect_error', (err) => {
+      console.error('client connection error', err);
+    });
+
+    socket.on('getRouterRtpCapabilities', (data, callback) => {
+      callback(mediasoupRouter.rtpCapabilities);
+    });
+
+    socket.on('createProducerTransport', async (data, callback) => {
+      try {
+        const { transport, params } = await createWebRtcTransport();
+        producerTransport = transport;
+        callback(params);
+      } catch (err) {
+        console.error(err);
+        callback({ error: err.message });
+      }
+    });
+
+    socket.on('createConsumerTransport', async (data, callback) => {
+      try {
+        const { transport, params } = await createWebRtcTransport();
+        consumerTransport = transport;
+        callback(params);
+      } catch (err) {
+        console.error(err);
+        callback({ error: err.message });
+      }
+    });
+
+    socket.on('connectProducerTransport', async (data, callback) => {
+      await producerTransport.connect({ dtlsParameters: data.dtlsParameters });
+      callback();
+    });
+
+    socket.on('connectConsumerTransport', async (data, callback) => {
+      await consumerTransport.connect({ dtlsParameters: data.dtlsParameters });
+      callback();
+    });
+
+    socket.on('produce', async (data, callback) => {
+      const {kind, rtpParameters} = data;
+      producer = await producerTransport.produce({ kind, rtpParameters });
+      callback({ id: producer.id });
+
+      // inform clients about new producer
+      socket.broadcast.emit('newProducer');
+    });
+
+    socket.on('consume', async (data, callback) => {
+      callback(await createConsumer(producer, data.rtpCapabilities));
+    });
+
+    socket.on('resume', async (data, callback) => {
+      await consumer.resume();
+      callback();
+    });
+  });
+}
+
+async function runMediasoupWorker() {
+  worker = await mediasoup.createWorker({
+    logLevel: config.mediasoup.worker.logLevel,
+    logTags: config.mediasoup.worker.logTags,
+    rtcMinPort: config.mediasoup.worker.rtcMinPort,
+    rtcMaxPort: config.mediasoup.worker.rtcMaxPort,
+  });
+
+  worker.on('died', () => {
+    console.error('mediasoup worker died, exiting in 2 seconds... [pid:%d]', worker.pid);
+    setTimeout(() => process.exit(1), 2000);
+  });
+
+  const mediaCodecs = config.mediasoup.router.mediaCodecs;
+  mediasoupRouter = await worker.createRouter({ mediaCodecs });
+}
+
+async function createWebRtcTransport() {
+  const {
+    maxIncomingBitrate,
+    initialAvailableOutgoingBitrate
+  } = config.mediasoup.webRtcTransport;
+
+  const transport = await mediasoupRouter.createWebRtcTransport({
+    listenIps: config.mediasoup.webRtcTransport.listenIps,
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+    initialAvailableOutgoingBitrate,
+  });
+  if (maxIncomingBitrate) {
+    try {
+      await transport.setMaxIncomingBitrate(maxIncomingBitrate);
+    } catch (error) {
+    }
+  }
+  return {
+    transport,
+    params: {
+      id: transport.id,
+      iceParameters: transport.iceParameters,
+      iceCandidates: transport.iceCandidates,
+      dtlsParameters: transport.dtlsParameters
+    },
+  };
+}
+
+async function createConsumer(producer, rtpCapabilities) {
+  if (!mediasoupRouter.canConsume(
+    {
+      producerId: producer.id,
+      rtpCapabilities,
+    })
+  ) {
+    console.error('can not consume');
+    return;
+  }
+  try {
+    consumer = await consumerTransport.consume({
+      producerId: producer.id,
+      rtpCapabilities,
+      paused: producer.kind === 'video',
+    });
+  } catch (error) {
+    console.error('consume failed', error);
+    return;
+  }
+
+  if (consumer.type === 'simulcast') {
+    await consumer.setPreferredLayers({ spatialLayer: 2, temporalLayer: 2 });
+  }
+
+  return {
+    producerId: producer.id,
+    id: consumer.id,
+    kind: consumer.kind,
+    rtpParameters: consumer.rtpParameters,
+    type: consumer.type,
+    producerPaused: consumer.producerPaused
+  };
+}
