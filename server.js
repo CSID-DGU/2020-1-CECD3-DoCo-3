74dfac9ae256f11a0079141c3ea22a3f46f19624
heechul <khc4190@naver.com>
2020-11-07 20:27:58 +0900

    a

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/createRoom", async (req, res, next) => {
   const mediasoupRouter = await worker.createRouter({ mediaCodecs });
   // Might need to put below into database?
   rooms[mediasoupRouter.id] = new Room(mediasoupRouter.id, mediasoupRouter);
-  res.json({roomId: mediasoupRouter.id, rs: rooms});
+  res.json({roomId: mediasoupRouter.id});
 });
 
 app.get("/roomExists", async (req, res, next) => {
