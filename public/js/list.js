acc562017e7484af383054f4ca0f4d71637a8dae
hunmoyang <didgnsah7320@gmail.com>
2020-11-08 15:09:45 +0900

    ejs

--- public/js/list.js
+++ public/js/list.js
@@ -10,26 +10,16 @@ let toRoom = []; //Room 저장하는 변수
 //방 생성 시 
 createBtn.addEventListener("submit", function(e){
 
-    if(roomName.value === ''){
-
-        alert("방 이름을 입력하시오!");
-    }
-    else{
-
-        paintRoom(roomName.value);     
-
-    }
-
+    paintRoom();
     e.preventDefault();
+    
+
 });
 
 //룸 그리기 
-function paintRoom(text){
+function paintRoom(){
 
-    var data  = {
-        title : text
-    };
-    var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() { // 요청에 대한 콜백
     if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
     if (xhr.status === 200 || xhr.status === 201) {
@@ -39,10 +29,8 @@ function paintRoom(text){
     }
   }
     };
-    xhr.open('POST', 'https://docoex.page/createRoom'); // 메소드와 주소 설정
-    xhr.setRequestHeader('Content-Type', 'application/json'); // 컨텐츠타입을 json으로
-    xhr.send(JSON.stringify(data)); // 데이터를 stringify해서 보냄
-   
+    xhr.open('GET', 'https://docoex.page/createRoom'); // 메소드와 주소 설정
+    xhr.send(); // 요청 전송 
 }
 
 
