ad1c15937e2c32b45068784fd7f3f15ca0120116
heechul <khc4190@naver.com>
2020-11-08 14:10:19 +0900

    test

--- require/roomList.js
+++ require/roomList.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, _) => {
-    var roomList = new Array();
-    for(var i = 0; i < rooms.length; i++){
-        console.log(rooms[i].roomId);
-    }
+    var roomList = [];
+    roomList.forEach(function(element){
+        roomList.push(element.roomId);
+    });
     res.json(roomList);
     //res.json({rs: rooms});
 });
