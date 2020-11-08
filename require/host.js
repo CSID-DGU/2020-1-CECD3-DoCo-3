a8f4f839ca9ec6e9194d3cf039b5e938f13ebb75
heechul <khc4190@naver.com>
2020-11-08 18:24:36 +0900

    transport

--- require/host.js
+++ require/host.js
@@ -10,6 +10,38 @@ router.get('/', async (req, res, _) => {
         res.send('CANNOT FIND')
         return 
     }
+
+    async function createWebRtcTransport(roomId) {
+        const {
+          maxIncomingBitrate,
+          initialAvailableOutgoingBitrate
+        } = config.mediasoup.webRtcTransport;
+      
+        const transport = await rooms[roomId].getRouter().createWebRtcTransport({
+          listenIps: config.mediasoup.webRtcTransport.listenIps,
+          enableUdp: true,
+          enableTcp: true,
+          preferUdp: true,
+          initialAvailableOutgoingBitrate,
+        });
+        console.log('Created WebRtcTransport...')
+        if (maxIncomingBitrate) {
+          try {
+            await transport.setMaxIncomingBitrate(maxIncomingBitrate);
+          } catch (error) {
+          }
+        }
+        return {
+          transport,
+          params: {
+            id: transport.id,
+            iceParameters: transport.iceParameters,
+            iceCandidates: transport.iceCandidates,
+            dtlsParameters: transport.dtlsParameters
+          },
+        };
+      }
+
     try {
         const { transport, params } = await createWebRtcTransport(currentRoom.roomId);
         currentRoom.addActiveProducerTransport(transport);
