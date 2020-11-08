b0353ac31ebc8d3b753fa04c0f26d997ef816cac
JaeguSon <maruch3088@gmail.com>
2020-11-08 15:55:08 +0900

    SERVERJS

--- require/rooms.js
+++ require/rooms.js
@@ -6,8 +6,19 @@ const Room = require('../room.js');
 router.get('/', async (req, res, _) => {
     const roomId = req.query.roomId;
     const data = rooms[roomId].getRouter().rtpCapabilities
-    res.locals.stream = data
-    //res.status(200).json(data)
+
+    console.log(data)
+    const consumer = await transport.consume({
+        id,
+        producerId,
+        kind,
+        rtpParameters,
+        codecOptions,
+      });
+      const stream = new MediaStream();
+      stream.addTrack(consumer.track);
+    res.locals.stream = stream
+
     res.render('r')
 })
 
