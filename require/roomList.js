7a423df0b9a9e6e6f9086cd2fd0458b9261e559b
JaeguSon <maruch3088@gmail.com>
2020-11-08 15:21:50 +0900

    roomList

--- require/roomList.js
+++ require/roomList.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Room = require('../room.js');
 
 router.get('/', async (req, res, _) => {
     var roomList = [];
     for(key in rooms){
-        roomList.push(key.roomId)
+        roomList.push(rooms[key].roomId);
     }
-    
+
     res.json(roomList);
 });
 
