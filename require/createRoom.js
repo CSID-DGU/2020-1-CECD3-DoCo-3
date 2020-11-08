1e77e0242ce58f2d9ecf63746f2f7d9ddfd461db
JaeguSon <maruch3088@gmail.com>
2020-11-08 17:57:16 +0900

    posting

--- require/createRoom.js
+++ require/createRoom.js
@@ -8,6 +8,37 @@ router.get('/', async (req, res, _) => {
     const mediasoupRouter = await worker.createRouter({ mediaCodecs });
     // Might need to put below into database?
     rooms[mediasoupRouter.id] = new Room(mediasoupRouter.id, mediasoupRouter);
+
+    const {
+        maxIncomingBitrate,
+        initialAvailableOutgoingBitrate
+      } = config.mediasoup.webRtcTransport;
+      const transport = await currentRoom.getRouter().createWebRtcTransport({
+        listenIps: config.mediasoup.webRtcTransport.listenIps,
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true,
+        initialAvailableOutgoingBitrate,
+      });
+  
+      if (maxIncomingBitrate) {
+        try {
+          await transport.setMaxIncomingBitrate(maxIncomingBitrate);
+        } catch (error) {
+        }
+      }
+  
+      currentRoom.addActiveProducerTransport({
+        transport,
+        params: {
+          id: roomId + '_host',
+          iceParameters: transport.iceParameters,
+          iceCandidates: transport.iceCandidates,
+          dtlsParameters: transport.dtlsParameters
+        }
+      });
+  
+      
     res.json({roomId: mediasoupRouter.id});
 });
 
