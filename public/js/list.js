9425b170971d1023f4c5df44e204aa55958cdc72
hunmoyang <didgnsah7320@gmail.com>
2020-11-08 16:48:05 +0900

    tt

--- public/js/list.js
+++ public/js/list.js
@@ -18,7 +18,7 @@ function paintRoom(){
     if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
     if (xhr.status === 200 || xhr.status === 201) {
       const Room = JSON.parse(xhr.responseText);
-      location.href = `https://docoex.page/index?${Room.roomId}`;
+      location.href = `https://docoex.page/host?${Room.roomId}`;
     } else {
       console.error(xhr.responseText);
     }
