ae5f37f3af80ec59ab7e949f5cd90a523f7a441c
heechul <khc4190@naver.com>
2020-11-08 20:10:01 +0900

    asd

--- public/js/host.js
+++ public/js/host.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Room = require('../room.js');
+
 
 router.get('/', async (req, res, _) => {
     const roomId = req.query.roomId;
