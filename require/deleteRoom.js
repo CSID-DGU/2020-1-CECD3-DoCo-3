3d7b4bafaf986e76ed497ad8788e82cb3a5481f0
JaeguSon <maruch3088@gmail.com>
2020-11-08 15:25:35 +0900

    delete

--- require/deleteRoom.js
+++ require/deleteRoom.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res, _) => {
-    console.log(rooms)
+    const id = req.query.roomId
+    console.log(id + '/' + rooms.array);
     res.send('ERASE')
 });
-
 module.exports = router
