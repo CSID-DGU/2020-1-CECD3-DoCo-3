dfec9a50bcbb0f56e18bc05afb33b663ffa97775
JaeguSon <maruch3088@gmail.com>
2020-11-08 15:27:38 +0900

    typeof

--- require/deleteRoom.js
+++ require/deleteRoom.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', async (req, res, _) => {
     const id = req.query.roomId
-    console.log(id + '/' + rooms.array);
+    console.log(id + typeof(rooms));
     res.send('ERASE')
 });
 module.exports = router
