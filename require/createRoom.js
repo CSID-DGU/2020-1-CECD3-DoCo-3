e8705477a54df029d0b70ce93ec549beb14ff6d2
heechul <khc4190@naver.com>
2020-11-17 20:33:34 +0900

    asd

--- require/createRoom.js
+++ require/createRoom.js
@@ -3,6 +3,39 @@ const express = require('express');
 const router = express.Router();
 const Room = require('../room.js');
 
+async function createWebRtcTransport(roomId) {
+    const {
+        maxIncomingBitrate,
+        initialAvailableOutgoingBitrate
+    } = config.mediasoup.webRtcTransport;
+  
+    const transport = await rooms[roomId].getRouter().createWebRtcTransport({
+        listenIps: config.mediasoup.webRtcTransport.listenIps,
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true,
+        initialAvailableOutgoingBitrate,
+    });
+    console.log('Created WebRtcTransport...')
+    if (maxIncomingBitrate) {
+        try {
+            await transport.setMaxIncomingBitrate(maxIncomingBitrate);
+        } catch (error) {
+        }
+    }
+
+    //console.log(transport)
+    return {
+        transport,
+        params: {
+            id: transport.id,
+            iceParameters: transport.iceParameters,
+            iceCandidates: transport.iceCandidates,
+            dtlsParameters: transport.dtlsParameters
+        },
+    };
+}
+
 router.get('/', async (req, res, _) => {
     const mediaCodecs = config.mediasoup.router.mediaCodecs;
     const mediasoupRouter = await worker.createRouter({ mediaCodecs });
@@ -13,8 +46,8 @@ router.get('/', async (req, res, _) => {
     rooms[mediasoupRouter.id].addActiveProducerTransport(transport);
 
     res.redirect('/host?roomId='+ mediasoupRouter.id + '&prodId='+ transport.id);
+    //res.json({roomId: mediasoupRouter.id, prodId: transport.id});
 });
-
 
 
 
