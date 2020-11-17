3454aba37918bffa88cd840133fa074892c2c9e6
JaeguSon <maruch3088@gmail.com>
2020-11-17 19:36:28 +0900

    req.body

--- host.js
+++ host.js
@@ -1,6 +1,7 @@
 const mediasoup = require('mediasoup-client');
 const socketClient = require('socket.io-client');
 const socketPromise = require('./lib/socket.io-promise').promise;
+const $ = document.querySelector.bind(document);
 
 async function publish(stream, id) {
     transport.on('produce', async ({ kind, rtpParameters }, callback, errback) => {
