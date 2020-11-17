29ace2153a758ac79c11da15384f891f2846f788
jinwon <prime4865@gmail.com>
2020-11-17 18:51:34 +0900

    aaa

---
+++ require/client.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const Room = require('../room.js');
+
+router.get('/', async (req, res, _) => {
+    const roomId = req.query.roomId;
+    const prodId = req.query.prodId;
+  
+    if (rooms[roomId] === undefined) { 
+        res.send('CANNOT FIND')
+        return 
+    }  
+
+    res.locals.rid = roomId
+    res.locals.pid = prodId
+
+    res.render('host')
+})
+
+
+router.post('/', async (req, res, _) => {
+  const stream = req.body.stream
+  const prodId = req.body.prodId
+  const roomId = req.body.roomId
+
+
+  console.log(req.body)
+  if (rooms[roomId] === undefined) { 
+      res.send('CANNOT FIND')
+      return 
+  }  
+
+  const transport = rooms[roomId].getActiveProducerTransport(prodId)
+  const producer = await transport.produce({ stream })
+  rooms[roomId].addActiveProducerToTransport(prodId, producer)
+  
+  res.send('comp')
+})
+
+module.exports = router;
