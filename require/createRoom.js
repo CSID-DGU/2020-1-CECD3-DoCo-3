9971acf8dee5dbd097fb05e3ad87694780d49fa3
heechul <khc4190@naver.com>
2020-11-17 20:30:01 +0900

    sad

--- require/createRoom.js
+++ require/createRoom.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const Room = require('../room.js');
 
 router.get('/', async (req, res, _) => {
-    const name = req.query.roomName;
     const mediaCodecs = config.mediasoup.router.mediaCodecs;
     const mediasoupRouter = await worker.createRouter({ mediaCodecs });
-    // Might need to put below into database?
-    rooms[name] = new Room(mediasoupRouter.id, mediasoupRouter);
-    res.json({name});
+    
+    rooms[mediasoupRouter.id] = new Room(mediasoupRouter.id, mediasoupRouter);
+
+    const { transport, params } = await createWebRtcTransport(mediasoupRouter.id)
+    rooms[mediasoupRouter.id].addActiveProducerTransport(transport);
+
+    res.redirect('/host?roomId='+ mediasoupRouter.id + '&prodId='+ transport.id);
 });
 
 
