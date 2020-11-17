2d64065a9097176194bd31b18cd6aeb0b46005fd
heechul <khc4190@naver.com>
2020-11-17 20:56:50 +0900

    asd

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(server, options);
 const Room = require('./room.js');
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 const cors = require('cors')
 const corsOptions = {
@@ -40,7 +40,7 @@ app.use("/createRoom",  require('./require/createRoom.js'));
 app.use("/existRoom",   require('./require/existsRoom.js'));
 app.use("/deleteRoom",  require('./require/deleteRoom.js'));
 app.use('/room',        require('./require/rooms.js'));
-//app.use('/host',        require('./require/host.js'));
+app.use('/host',        require('./require/host.js'));
 app.use('/roomList',    require('./require/roomList.js'));
 
 app.get('/', function(req,res){ res.render('list', {}); });
