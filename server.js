bafcb93e81a49c029c049ce72bf7e823f38c48e3
JaeguSon <maruch3088@gmail.com>
2020-11-18 02:44:43 +0900

    hostrouterobj

--- server.js
+++ server.js
@@ -106,6 +106,8 @@ async function runMediasoupWorker() {
     console.error('mediasoup worker died, exiting in 2 seconds... [pid:%d]', worker.pid);
     setTimeout(() => process.exit(1), 2000);
   });
+
+  console.log('Worker is ready')
 }
 
 async function runSocketServer() {
