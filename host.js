b955f63e6145c597c0e7a628e6f6dbdc37562b31
JaeguSon <maruch3088@gmail.com>
2020-11-17 19:54:49 +0900

    req.body

--- host.js
+++ host.js
@@ -6,26 +6,115 @@ const $ = document.querySelector.bind(document);
 const $btnWebcam = $('#btn_webcam');
 $btnWebcam.addEventListener('click', publish);
 
-async function publish(stream, id) {
-  console.log($)
+// async function publish(stream, id) {
+//   console.log($)
 
-    transport.on('produce', async ({ kind, rtpParameters }, callback, errback) => {
-      try {
-        const { id } = await socket.request('produce', {
-          transportId: transport.id,
-          kind,
-          rtpParameters,
-        });
-      } catch (err) {
-        errback(err);
-      }
-    });
+//     transport.on('produce', async ({ kind, rtpParameters }, callback, errback) => {
+//       try {
+//         const { id } = await socket.request('produce', {
+//           transportId: transport.id,
+//           kind,
+//           rtpParameters,
+//         });
+//       } catch (err) {
+//         errback(err);
+//       }
+//     });
 
 
-    try {
-      const track = stream.getVideoTracks()[0];
-      const params = { track };
+//     try {
+//       const track = stream.getVideoTracks()[0];
+//       const params = { track };
       
-      producer = await transport.produce(params);
-    } catch (err) { }
+//       producer = await transport.produce(params);
+//     } catch (err) { }
+// }
+
+
+let device;
+let socket;
+
+async function connect() {
+
+  const opts = {
+    path: '/rooms',
+    transports: ['websocket'],
+  };
+
+  const serverUrl = `https://docoex.page`;
+  socket = socketClient(serverUrl, opts);
+  console.log(socket);
+  socket.request = socketPromise(socket);
+
+  socket.on('connect', async () => {
+    const data = await socket.request('getRouterRtpCapabilities');
+    await loadDevice(data);
+  });
+
+
+  socket.on('connect_error', (error) => {
+    console.error('could not connect to %s%s (%s)', serverUrl, opts.path, error.message);
+  });
+
+}
+
+async function loadDevice(routerRtpCapabilities) {
+  try {
+    device = new mediasoup.Device();
+  } catch (error) {
+    if (error.name === 'UnsupportedError') {
+      console.error('browser not supported');
+    }
+  }
+  await device.load({ routerRtpCapabilities });
+}
+
+async function publish() {
+  const data = await socket.request('createProducerTransport', {
+    forceTcp: false,
+    rtpCapabilities: device.rtpCapabilities,
+  });
+  console.log(data);
+  if (data.error) {
+    console.error(data.error);
+    return;
+  }
+
+  const transport = device.createSendTransport(data);
+  transport.on('connect', async ({ dtlsParameters }, callback, errback) => {
+    socket.request('connectProducerTransport', { dtlsParameters })
+      .then(callback)
+      .catch(errback);
+  });
+
+  transport.on('produce', async ({ kind, rtpParameters }, callback, errback) => {
+    try {
+      const { id } = await socket.request('produce', {
+        transportId: transport.id,
+        kind,
+        rtpParameters,
+      });
+      callback({ id });
+    } catch (err) {
+      errback(err);
+    }
+  });
+
+  let stream;
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({ video: true, audio : true }) 
+    const track = stream.getVideoTracks()[0];
+    const params = { track };
+    params.encodings = [
+      { maxBitrate: 100000 },
+      { maxBitrate: 300000 },
+      { maxBitrate: 900000 },
+    ];
+    params.codecOptions = {
+      videoGoogleStartBitrate : 1000
+    };
+    producer = await transport.produce(params);
+  } catch (err) {
+    console.log(err)
+  }
 }
