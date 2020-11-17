2f7e725ba78d19fd3147614609eeaf02f35280d3
heechul <khc4190@naver.com>
2020-11-17 20:53:54 +0900

    asd

--- require/host.js
+++ require/host.js
@@ -3,59 +3,18 @@ const router = express.Router();
 const socketPromise = require('../lib/socket.io-promise').promise;
 
 router.get('/', async (req, res, _) => {
-    const roomId = req.query.roomId;
-    const currentRoom = rooms[roomId]
-    if (currentRoom === undefined) { 
-        res.send('CANNOT FIND')
-        return 
-    }  
+  const roomId = req.query.roomId;
+  const prodId = req.query.prodId;
 
-    // const opts = {
-    //   path: '/rooms',
-    //   transports: ['websocket'],
-    // };
+  if (rooms[roomId] === undefined) { 
+      res.send('CANNOT FIND')
+      return 
+  }  
 
-    // const hostname = window.location.hostname;
-    // const serverUrl = `https://${hostname}`;
-    // socket = socketClient(serverUrl, opts);
-    // console.log(socket);
-    // socket.request = socketPromise(socket);
-  
-    // socket.on('connect', async () => {
-    //   const data = await socket.request('getRouterRtpCapabilities');
-    //   await loadDevice(data);
-    // });
-  
-    // socket.on('connect_error', (error) => {
-    //   console.error('could not connect to %s%s (%s)', serverUrl, opts.path, error.message);
-    // });
-        
-    // async function loadDevice(routerRtpCapabilities) {
-    //   try {
-    //     device = new mediasoup.Device();
-    //   } catch (error) {
-    //     if (error.name === 'UnsupportedError') {
-    //       console.error('browser not supported');
-    //     }
-    //   }
-    //   await device.load({ routerRtpCapabilities });
-    // }
+  res.locals.rid = roomId
+  res.locals.pid = prodId
 
-    // try {
-      
-    //   const track = clientStream.getVideoTracks()[0];
-    //   const params = { track };
-    //     params.codecOptions = {
-    //       videoGoogleStartBitrate : 1000
-    //   }
-    //   producer = await currentRoom.getActiveProducerTransport().produce(params);
-    // } catch (err) {
-    //     console.log(err)
-    //         return
-    //   //$txtPublish.innerHTML = 'failed';
-    // }
-    
-    res.redirect('/host?roomId='+ roomId);
+  res.render('host')
 })
 
 module.exports = router;
