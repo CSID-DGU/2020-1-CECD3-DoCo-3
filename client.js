b0cee01b965ca5cad3c989f06aaf2095791152c6
JaeguSon <maruch3088@gmail.com>
2020-11-18 02:36:10 +0900

    hostrouterobj

--- client.js
+++ client.js
@@ -25,7 +25,7 @@ let $txtPublish;
 
 $btnConnect.addEventListener('click', create);
 $btnWebcam.addEventListener('click', publish);
-$btnSubscribe.addEventListener('click', subscribe);
+$btnSubscribe.addEventListener('click', subscribe_b);
 $btnList.addEventListener('click', initialize);
 
 if (typeof navigator.mediaDevices.getDisplayMedia === 'undefined') {
@@ -219,6 +219,29 @@ async function getUserMedia(transport, isWebcam) {
   return stream;
 }
 
+function subscribe_b() {
+  const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() { // 요청에 대한 콜백
+    if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
+    if (xhr.status === 200 || xhr.status === 201) {
+      const data = JSON.parse(xhr.responseText);
+      console.log(data.exists)
+      console.log(data.clientId)
+      if (data.exists === 'true') {
+        sessionStorage.setItem('CLIENTID', Room.clientId);
+        subscribe();
+      }
+
+      
+    } else {
+      console.error(xhr.responseText);
+    }
+  }
+    };
+    xhr.open('GET', 'https://docoex.page/roomExists?roomId='+sessionStorage.getItem('ROOMID')); // 메소드와 주소 설정
+    xhr.send(); // 요청 전송 
+}
+
 async function subscribe() {
   const data = await socket.request('createConsumerTransport', {
     roomId : sessionStorage.getItem('ROOMID'),
@@ -268,26 +291,6 @@ async function subscribe() {
   });
 
   const stream = consume(transport);
-
-  const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() { // 요청에 대한 콜백
-    if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
-    if (xhr.status === 200 || xhr.status === 201) {
-      const data = JSON.parse(xhr.responseText);
-      console.log(data.exists)
-      console.log(data.clientId)
-      if (data.exists === 'true') {
-        sessionStorage.setItem('CLIENTID', Room.clientId);
-      }
-
-      
-    } else {
-      console.error(xhr.responseText);
-    }
-  }
-    };
-    xhr.open('GET', 'https://docoex.page/roomExists?roomId='+sessionStorage.getItem('ROOMID')); // 메소드와 주소 설정
-    xhr.send(); // 요청 전송 
 }
 
 async function consume(transport) {
