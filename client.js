3b86ba9a1de9340718b533a4779f59f5ba5ae78b
JaeguSon <maruch3088@gmail.com>
2020-11-18 01:18:52 +0900

    mediaCodes

--- client.js
+++ client.js
@@ -72,6 +72,22 @@ async function connect() {
   socket.on('newProducer', () => {
     $fsSubscribe.disabled = false;
   });
+
+  const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() { // 요청에 대한 콜백
+    if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
+    if (xhr.status === 200 || xhr.status === 201) {
+      const Room = JSON.parse(xhr.responseText);
+      console.log(Room.roomId)
+
+      sessionStorage.setItem('ROOMID', Room.roomId);
+    } else {
+      console.error(xhr.responseText);
+    }
+  }
+    };
+    xhr.open('GET', 'https://docoex.page/createRoom'); // 메소드와 주소 설정
+    xhr.send(); // 요청 전송 
 }
 
 async function loadDevice(routerRtpCapabilities) {
