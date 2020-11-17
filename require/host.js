7c5f61b7fcec4a1b3ec9496b8b34a67c9f26104f
heechul <khc4190@naver.com>
2020-11-17 20:42:32 +0900

    asd

--- require/host.js
+++ require/host.js
@@ -10,54 +10,54 @@ router.get('/', async (req, res, _) => {
         return 
     }  
 
-    const opts = {
-      path: '/rooms',
-      transports: ['websocket'],
-    };
+    // const opts = {
+    //   path: '/rooms',
+    //   transports: ['websocket'],
+    // };
 
-    const hostname = window.location.hostname;
-    const serverUrl = `https://${hostname}`;
-    socket = socketClient(serverUrl, opts);
-    console.log(socket);
-    socket.request = socketPromise(socket);
+    // const hostname = window.location.hostname;
+    // const serverUrl = `https://${hostname}`;
+    // socket = socketClient(serverUrl, opts);
+    // console.log(socket);
+    // socket.request = socketPromise(socket);
   
-    socket.on('connect', async () => {
-      const data = await socket.request('getRouterRtpCapabilities');
-      await loadDevice(data);
-    });
+    // socket.on('connect', async () => {
+    //   const data = await socket.request('getRouterRtpCapabilities');
+    //   await loadDevice(data);
+    // });
   
-    socket.on('connect_error', (error) => {
-      console.error('could not connect to %s%s (%s)', serverUrl, opts.path, error.message);
-    });
+    // socket.on('connect_error', (error) => {
+    //   console.error('could not connect to %s%s (%s)', serverUrl, opts.path, error.message);
+    // });
         
-    async function loadDevice(routerRtpCapabilities) {
-      try {
-        device = new mediasoup.Device();
-      } catch (error) {
-        if (error.name === 'UnsupportedError') {
-          console.error('browser not supported');
-        }
-      }
-      await device.load({ routerRtpCapabilities });
-    }
+    // async function loadDevice(routerRtpCapabilities) {
+    //   try {
+    //     device = new mediasoup.Device();
+    //   } catch (error) {
+    //     if (error.name === 'UnsupportedError') {
+    //       console.error('browser not supported');
+    //     }
+    //   }
+    //   await device.load({ routerRtpCapabilities });
+    // }
 
-    try {
+    // try {
       
-      const track = clientStream.getVideoTracks()[0];
-      const params = { track };
-        params.codecOptions = {
-          videoGoogleStartBitrate : 1000
-      }
-      producer = await currentRoom.getActiveProducerTransport().produce(params);
-    } catch (err) {
-        console.log(err)
-            return
-      //$txtPublish.innerHTML = 'failed';
-    }
+    //   const track = clientStream.getVideoTracks()[0];
+    //   const params = { track };
+    //     params.codecOptions = {
+    //       videoGoogleStartBitrate : 1000
+    //   }
+    //   producer = await currentRoom.getActiveProducerTransport().produce(params);
+    // } catch (err) {
+    //     console.log(err)
+    //         return
+    //   //$txtPublish.innerHTML = 'failed';
+    // }
     
     
 
-    res.render('host')
+    res.send();
 })
 
 module.exports = router;
