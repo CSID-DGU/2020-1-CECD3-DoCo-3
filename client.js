ea46c5542774e3327c1c9aea7234772f484a0996
hunmoyang <didgnsah7320@gmail.com>
2020-11-18 16:12:26 +0900

    tew

--- client.js
+++ client.js
@@ -57,6 +57,7 @@ function create() {
       if (xhr.readyState === xhr.DONE) { // 요청이 완료되면
         if (xhr.status === 200 || xhr.status === 201) {
           const Room = JSON.parse(xhr.responseText);
+          console.log(Room);
           console.log(Room.roomId)
 
           sessionStorage.setItem('ROOMID', Room.roomId);
@@ -322,4 +323,3 @@ async function consume(transport) {
   stream.addTrack(consumer.track);
   return stream;
 }
-
