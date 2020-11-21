0cc8cead70d205877ab19b8961b5419a7c79e290
heechul <khc4190@naver.com>
2020-11-22 03:57:12 +0900

    asD

--- client.js
+++ client.js
@@ -353,7 +353,7 @@ async function subscribeh(cid, cnt) {
       case 'connected':
         //const s = document.getElementById('remote_video_' + cnt)
         
-        document.querySelector('#guest_video_0').srcObject = await streams[cid];
+        document.querySelector('#remote_video_0').srcObject = await streams[cid];
         console.log('STREAM DATAS : : : :' + await streams[cid])
         await socket.request('resume');
         break;
